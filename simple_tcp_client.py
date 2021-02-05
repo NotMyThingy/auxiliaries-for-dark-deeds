00d6741b4e3aa4c9096fd6bbe7aad277fb116173
NotMyThingy <miska.janhunen@protonmail.com>
2021-02-05 16:22:08 +0200

    simple tcp client

---
+++ simple_tcp_client.py
@@ -0,0 +1,19 @@
+import socket
+
+RHOST = "www.google.com"
+RPORT = 80
+
+# create a socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((RHOST, RPORT))
+
+# send data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# receive data
+response = client.recv(4096)
+
+print(response.decode())
+client.close
